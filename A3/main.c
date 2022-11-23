fa02fa0ae7418f3ef34f73b10532d8f38d245780
RichmondAB <107730224+RichmondAB@users.noreply.github.com>
2022-11-23 16:04:04 -0500

    working uncommented code

--- A3/main.c
+++ A3/main.c
@@ -2,7 +2,7 @@
 
 sem_t vocal, composer, room, mutex, composerRoom, vocalRoom;
 
-int maxWanderTime = 0, maxSoundRoomTime = 0, vocNum, compNum, totalRoomNum;
+int maxWanderTime = 0, maxSoundRoomTime = 0, vocNum, compNum, totalRoomNum, claimedVoc = 0, claimedComp = 0;
 
 Queue vocalNums, compNums;
 
@@ -16,9 +16,21 @@ void random_sleep(int max) {
 void room_task(int num) {
 	
 	while(1) {
-		if (vocalNums.length <= 0 || compNums.length <= 0)
-			exit(0);
 		
+		sem_wait(&mutex);
+		if(claimedVoc == vocNum || claimedComp == compNum) {
+
+			sem_post(&mutex);
+			pthread_exit(0);
+
+		}
+		else {
+
+			claimedVoc++;
+			claimedComp++;
+			sem_post(&mutex);
+
+		}
 		sem_wait(&vocal);
 		sem_wait(&composer);
 		sem_wait(&mutex);
@@ -29,9 +41,8 @@ void room_task(int num) {
 		sem_post(&composerRoom);
 		sem_post(&vocalRoom);
 		
-		
 	}
-	
+
 }
 
 void voc_task(int num) {
@@ -46,6 +57,7 @@ void voc_task(int num) {
 	sem_post(&vocal);
 	sem_wait(&vocalRoom);
 	printf("Vocalist %d: I am done now. Goodbye...\n", num);
+	pthread_exit(0);
 	
 }
 
@@ -61,15 +73,16 @@ void comp_task(int num) {
 	sem_post(&composer);
 	sem_wait(&composerRoom);
 	printf("Composer %d: I am done now. Goodbye...\n", num);
+	pthread_exit(0);
 	
 }
 
 void initialize_global(char ** argv) {
 	
-	sem_init(&vocal, 0, 1);
-	sem_init(&composer, 0 ,1);
-	sem_init(&composerRoom, 0, 1);
-	sem_init(&vocalRoom, 0, 1);
+	sem_init(&vocal, 0, 0);
+	sem_init(&composer, 0, 0);
+	sem_init(&composerRoom, 0, 0);
+	sem_init(&vocalRoom, 0, 0);
 	sem_init(&mutex, 0, 1);
 	
 	char * option = argv[1];
@@ -98,22 +111,25 @@ int main(int argc, char** argv) {
 	for (int vocIdx = 0; vocIdx < vocNum; vocIdx++){
 		
 		pthread_create(&vocalists[vocIdx], NULL, voc_task, (void *) vocIdx);
-		
+
 	}
 	
 	for (int compIdx = 0; compIdx < compNum; compIdx++) {
 		
 		pthread_create(&composers[compIdx], NULL, comp_task, (void*) compIdx);
-		
+
 	}
 	
 	for (int roomIdx = 0; roomIdx < totalRoomNum; roomIdx++) {
 		
 		pthread_create(&rooms[roomIdx], NULL, room_task, NULL);
-		pthread_join(&rooms[roomIdx], NULL);
 		
 	}
+
+	for (int joinIdx = 0; joinIdx < totalRoomNum; joinIdx++) {
+		pthread_join(rooms[joinIdx], NULL);
+	}
 	
-	
-	
+	exit(0);	
+
 }
